351f63ac108655d6f6804bbd955e7cad9e030c84
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-25 21:15:25 +0100

    update
    - allow multiple branches
    - option to specify prefix and suffix
    - option to delete head branches of specified PRs via numbers

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
-# Delete branch Github Action
+# Delete multiple branches Github Action
 
-An action that simply deletes a branch from repository.
+An action that deletes multiple branches from repository.
+Optionally one can provide a `prefix` or `suffix` strings that would be appended or prepended to every branch name.
 
 ## Usage
 
 ```yaml
-- name: Delete PR head branch
+- name: Delete PRs head branches
   uses: dawidd6/action-delete-branch@master
   with:
     github_token: ${{ secrets.GITHUB_TOKEN }}
-    branch: ${{ github.pull_request.head.ref }}
-    be_kind: true # don't fail on errors (optional)
+    numbers: 13,22,${{ github.event.pull_request.number }}
+- name: Delete pr-* branches
+  uses: dawidd6/action-delete-branch@master
+  with:
+    github_token: ${{ secrets.GITHUB_TOKEN }}
+    branches: 13,22,33
+    prefix: 'pr-'
+- name: Delete branch
+  uses: dawidd6/action-delete-branch@master
+  with:
+    github_token: ${{ secrets.GITHUB_TOKEN }}
+    branches: test
+    suffix: '-done'
 ```
--- entrypoint.rb
+++ entrypoint.rb
@@ -8,21 +8,46 @@ require 'octokit'
 repo = ENV["GITHUB_REPOSITORY"]
 
 # Inputs
-be_kind = ENV["INPUT_BE_KIND"]
-branch = ENV["INPUT_BRANCH"]
 token = ENV["INPUT_GITHUB_TOKEN"]
+branches = ENV["INPUT_BRANCHES"]
+numbers = ENV["INPUT_NUMBERS"]
+prefix = ENV["INPUT_PREFIX"]
+suffix = ENV["INPUT_SUFFIX"]
 
-# Main
-begin
-  client = Octokit::Client.new(:access_token => token)
-  response = client.delete_branch(repo, branch)
-  if response
-    puts "==> Branch \"#{branch}\" deleted successfully"
-  else
-    puts "==> Branch \"#{branch}\" deletion failure"
-    exit(1) unless be_kind
-  end
-rescue StandardError => e
-  puts "==> Error: #{e.message}"
-  exit(1) unless be_kind
+# Declare array with branches to delete
+branches_to_delete = []
+
+# Initialize octokit
+client = Octokit::Client.new(:access_token => token)
+
+# Append branches if any
+branches&.split(",")&.each do |branch|
+  branch = prefix + branch if prefix
+  branch = branch + suffix if suffix
+  branches_to_delete << branch
 end
+
+# Get head branches of PRs via provided issues numbers
+numbers&.split(",")&.each do |number|
+  # Check if issue is a PR
+  issue = client.issue(repo, number)
+
+  # Next if issue is not a PR
+  next if issue && issue["pull_request"].nil?
+
+  # Append head branch of PR to others
+  branch = client.pull_request(repo, number)["head"]["ref"]
+  branch = prefix + branch if prefix
+  branch = branch + suffix if suffix
+  branches_to_delete << branch
+end
+
+branches_to_delete.each do |branch|
+  if client.delete_branch(repo, branch)
+    puts branch
+  else
+    puts branch + " <== failed"
+    exit 1
+  end
+end
+
